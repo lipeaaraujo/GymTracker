ed8f9d969a7d4986fcc60f410c817f50d59d6383
LipeAKugel <felipearaujodff@gmail.com>
2024-11-30 21:42:46 -0300

    getting exercises from api response in exercises page

--- gym-tracker-app/src/pages/Exercises/Exercises.js
+++ gym-tracker-app/src/pages/Exercises/Exercises.js
@@ -1,12 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CgGym } from "react-icons/cg";
 import { FaPlus } from "react-icons/fa";
 import ExerciseModal from "../../components/ExerciseModal";
 import ExerciseBox from "../../components/ExerciseBox";
+import axios from "../../api/axios";
+import useAuth from "../../hooks/useAuth";
+
+const EXERCISE_URL = "/exercise"
 
 function Exercises() {
 
+  const { auth } = useAuth();
+
+  const [exercises, setExercises] = useState([]);
   const [exerciseModal, setExerciseModal] = useState(false)
+  const [errMsg, setErrMsg] = useState("");
+
+  useEffect(() => {
+    const getExercises = async () => {
+      try {
+        const response = await axios.get(
+          `${EXERCISE_URL}`,
+          {
+            headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${auth.accessToken}` },
+            withCredentials: true
+          }
+        )
+        console.log(response?.data);
+        setExercises(response?.data);
+      } catch (err) {
+        console.log(err?.response);
+        if (err?.response) {
+          setErrMsg("Error getting exercises");
+        } else {
+          setErrMsg("No Server Response");
+        }
+      }
+    }
+    getExercises();
+  }, [])
 
   return (
     <>
@@ -14,7 +46,9 @@ function Exercises() {
 
       <div className="flex gap-4 flex-wrap">
 
-        <ExerciseBox icon={<CgGym />} name="Legpress" />
+        {exercises.map(exercise => (
+          <ExerciseBox icon={<CgGym />} name={exercise.name} />
+        ))}
 
         <button 
           className="bg-neutral-900 w-32 h-32 rounded-xl hover:bg-zinc-600 duration-300"
