e5d85bd1c5195130c9e972dc1bb0d582cdc52dfc
LipeAKugel <felipearaujodff@gmail.com>
2024-12-25 21:43:15 -0300

    fixing date formatting to UTC

--- gym-tracker-app/src/utils/dateUtils.js
+++ gym-tracker-app/src/utils/dateUtils.js
@@ -1,5 +1,5 @@
 export const formatDate = (date) => {
-  return Intl.DateTimeFormat("en-us", { dateStyle: "full" }).format(
+  return Intl.DateTimeFormat("en-us", { dateStyle: "full", timeZone: "UTC" }).format(
     new Date(date)
   );
 };
