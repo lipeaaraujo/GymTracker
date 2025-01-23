2e5d481ea59917bad3f5fac420b7e24f0db820a8
LipeAKugel <felipearaujodff@gmail.com>
2025-01-23 00:57:01 -0300

    adicionando frontend-url na env e em arquivo de allowedOrigins

--- api/src/config/allowedOrigins.js
+++ api/src/config/allowedOrigins.js
@@ -2,7 +2,8 @@ const allowedOrigins = [
   'https://www.yoursite.com',
   'http://127.00.0.1:4000',
   'http://localhost:4000',
-  'http://localhost:3000'
-]
+  'http://localhost:3000',
+  process.env.FRONT_URL
+].filter(Boolean);
 
 module.exports = allowedOrigins;
