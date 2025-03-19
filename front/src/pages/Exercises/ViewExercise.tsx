d959253c5fe55cdcfc7d3ba3dc77d1b5a8e6aaa1
lipeaaraujo <felipearaujodff@gmail.com>
2025-03-19 01:21:06 -0300

    refactor: using components in ViewExercise page

--- front/src/pages/Exercises/ViewExercise.tsx
+++ front/src/pages/Exercises/ViewExercise.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import AddSessionDialog from "../../components/session/AddSessionDialog";
 import useExercise from "../../hooks/useExercise";
 import EditExerciseDialog from "../../components/exercises/EditExerciseDialog";
@@ -9,29 +9,20 @@ import IconButton from '@mui/material/IconButton';
 import ChevronLeftTwoToneIcon from '@mui/icons-material/ChevronLeftTwoTone';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
-import Divider from '@mui/material/Divider';
 import CardContent from '@mui/material/CardContent';
 import List from '@mui/material/List';
 import ListSubheader from '@mui/material/ListSubheader';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
-import ListItemText from '@mui/material/ListItemText';
-import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
 import { Exercise } from "../../types/exercise.types";
 import { toast } from "react-toastify";
 import CircularProgress from '@mui/material/CircularProgress';
-import Chip from '@mui/material/Chip';
-import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
-import EventNoteOutlinedIcon from '@mui/icons-material/EventNoteOutlined';
 import { formatDate } from "../../utils/dateUtils";
-import Button from '@mui/material/Button';
-import AddTwoToneIcon from '@mui/icons-material/AddTwoTone';
 import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 import DeleteExerciseDialog from "../../components/exercises/DeleteExerciseDialog";
 import EditDeleteActions from "../../components/EditDeleteActions";
 import useExerciseService from "../../api/exercise.service";
+import ListAddButton from "../../components/ListAddButton";
+import SessionItem from "../../components/session/SessionItem";
+import ExerciseCardDetails from "../../components/exercises/ExerciseCardDetails";
 
 const ViewExercise = () => {
   const { id } = useParams();
@@ -116,24 +107,11 @@ const ViewExercise = () => {
             }
           />
           <CardContent>
-            <Typography variant="body1" color="text.secondary">
-              { currentExercise.description }
-            </Typography>
-            <Divider sx={{ mt: 2, mb: 2 }} />
-            <Stack direction="row" gap={1}>
-              <Chip
-                icon={ <EmojiEventsIcon /> }
-                label={ `Personal Best: ${currentExercise.personalBest} kg` }
-                color="primary"
-                onClick={() => {}} // just a cool click effect
-              />
-              <Chip 
-                icon={ <EventNoteOutlinedIcon /> }
-                label={ `N° Sessions: ${currentExercise.sessions.length}` }
-                color="secondary"
-                onClick={() => {}} // just a cool click effect
-              />
-            </Stack>
+            <ExerciseCardDetails 
+              description={currentExercise.description}
+              personalBest={currentExercise.personalBest}
+              numOfSessions={currentExercise.sessions.length}
+            />
           </CardContent>
           <List
             subheader={
@@ -143,35 +121,17 @@ const ViewExercise = () => {
             } 
           >
             {currentExercise.sessions.map((session, i) => (
-              <ListItem
-                component={Link}
-                to={`/session/${session._id}`}
-                key={i}
-                disablePadding
-              >
-                <ListItemButton>
-                  <ListItemIcon>
-                    <CalendarMonthOutlinedIcon />
-                  </ListItemIcon>
-                  <ListItemText>
-                    {formatDate(session.date)}
-                  </ListItemText>
-                  <ArrowCircleRightIcon sx={{ color: "primary.main" }} />
-                </ListItemButton>
-              </ListItem>
+              <SessionItem
+                key={i} 
+                sessionId={session._id}
+                date={formatDate(session.date)}
+              />
             ))}
-            <ListItem key="add-button" alignItems="center" sx={{ display: "flex", justifyContent: "center" }}>
-              <Button
-                onClick={() => setSessionModal(true)}
-                variant="outlined"
-                sx={{
-                  gap: 1
-                }}
-              >
-                <AddTwoToneIcon />
-                ADD SESSION
-              </Button>
-            </ListItem>
+            <ListAddButton
+              key="add button"
+              onClick={() => setSessionModal(true)}
+              text="ADD SESSION"
+            />
           </List>
         </Card>
       )}
